a965744a455bf3c8aa39693c0cf661d08dcd30d9
Diego Rondón <72468973@ucsm.edu.pe>
2024-08-09 12:51:18 -0500

    Solucion de errores

--- src/Components/MarketingDigital/MarketingDigital.jsx
+++ src/Components/MarketingDigital/MarketingDigital.jsx
@@ -8,7 +8,7 @@ const MarketingDigital = () => {
       <div className="marketing-digital-text">
         <h1>Agencia de <br />Marketing</h1>
         <p>Construyendo sueños, creando conexiones.</p>
-        <p>Descubre quiénes somos y cómo damos vida a tu visión en <br />QOQAW</p>
+        <p>Descubre quiénes somos y cómo damos vida a tu visión en QOQAW</p>
       </div>
       <div className="marketing-digital-image">
       <img src={marketingdigital} alt="Marketing Digital" className="oval-image" />
